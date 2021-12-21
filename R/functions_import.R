07ef8a4adc5f5ae21f35c8bbce1ff0fb79156ce9
Cynthia Bello <cynthia.bello-hernandez@umontreal.ca>
2021-12-20 23:07:56 -0500

    import data
    imported the data from internet
    downloaded the data files into /data/raw

---
+++ R/functions_import.R
@@ -0,0 +1,41 @@
+# Script R qui contient les fonctions d'importation des données
+library(tidyverse)
+library(readxl)
+
+# URL des données 
+url_covid <- "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv"
+url_pib <- "https://docs.google.com/spreadsheets/d/1h3z8u0ykcUum8P9FV8EHF9fszDYr7iPDZQ-fgE3ecls/export?format=xlsx"
+url_life_ex <- "https://docs.google.com/spreadsheets/d/11mulzUH3_cueq-V9D5KIlo9oHE9YYZrUSeVyCin7_rM/export?format=xlsx"
+
+
+
+# check_filedate <- function(file, limite){
+#     files_list <- list.files("data/raw/",pattern=)
+#     date_file <- str_sub(filename, "data",".csv")
+# }
+
+download_data <- function(folder, name, url, ext){
+    filename <- paste0(folder, name, Sys.Date(),ext)
+    download.file(url, filename)
+}
+
+import_data <- function(filename, sheetname){
+    data <- read_excel(paste0("data/raw/", filename), sheet=sheetname)
+
+}
+
+download_data("data/raw/", "covid_data", url_covid,".csv")
+file_covid <- list.files("data/raw", pattern = "covid")
+covid_data <- read.csv(paste0("data/raw/", file_covid))
+
+download_data("data/raw/", "pib_data", url_pib,".xlsx")
+file_pib <- list.files("data/raw", pattern = "pib")
+pib_data <-import_data(file_pib, "data-for-countries-etc-by-year")
+
+download_data("data/raw/", "lifeexp_date", url_life_ex, ".xlsx")
+file_lifeexp <- list.files("data/raw", pattern = "lifeexp")
+lifeexp_data <- import_data(file_lifeexp, "data-for-countries-etc-by-year")
+
+
+
+
