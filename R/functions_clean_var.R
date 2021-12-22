39abd351cec90c5164b400e115be0de0f84be325
Cynthia Bello <cynthia.bello-hernandez@umontreal.ca>
2021-12-22 01:34:35 -0500

    render site to fix index.html
    ajout de la section "manipulation"
    régler bug avec index.html

--- R/functions_clean_var.R
+++ R/functions_clean_var.R
@@ -2,16 +2,19 @@
 
 # Colonnes des données Covid-19
 covid_data %<>% select(-c(8,9,15)) %>% mutate(date = ymd(date)) %>% 
-    relocate(date, .after = 2)
+    relocate(date, .after = 2)%>% 
+    rename(country = location)
+covid_data$country <- as.factor(covid_data$country)
 
 # Colonnes des données PIB
-
 pib_data %<>% rename(country = name,year = time, income_per_person = 4, gdp_total = 5) %>% 
     select(-6) 
- 
+pib_data$country <- as.factor(pib_data$country)
 
 # Colonnes des données Espérance de vie
-lifeexp_data %<>% rename(country = name,year = time,life_expectancy = `Life expectancy`)
+lifeexp_data %<>% rename(country = name,year = time,life_expectancy = `Life expectancy`) 
+lifeexp_data$country <- as.factor(lifeexp_data$country)
+
 
 
 
